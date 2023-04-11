4e530add11b48943ed329f6c51b60d6cb18bb529
miranda utami <mirandautami01@student.ciputra.ac.id>
2023-04-11 08:18:24 +0700

    Initial Commit

---
+++ 0706012110056-MirandaUtami-AFL3/_706012110056_MirandaUtami_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110056_MirandaUtami_AFL3App.swift
+//  0706012110056-MirandaUtami-AFL3
+//
+//  Created by miranda on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110056_MirandaUtami_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
